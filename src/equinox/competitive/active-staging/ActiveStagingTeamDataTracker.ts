e53df6e26965f54f5c93b68840000cab06160b54
Thiago Silva <thiagosilva.trab@gmail.com>
2026-07-14 18:01:16 -0300

    feat: trace active staging sets on team data

---
+++ src/equinox/competitive/active-staging/ActiveStagingTeamDataTracker.ts
@@ -0,0 +1,25 @@
+import type { ActiveStagingEngineInput } from './ActiveStagingEngineAdapter';
+
+export interface ActiveStagingTeamDataTrace {
+  competitiveVerificationState: 'staging-controlled';
+  expectedActiveV2SetsResolvedFromMongo: string[];
+  expectedActiveV2SetsPresentedToEngine: string[];
+  expectedActiveV2SetsAppliedToTeamData: string[];
+  localPilotFallbackUsed: false;
+}
+
+export type TeamDataWithActiveStagingTrace<T extends object> = T & ActiveStagingTeamDataTrace;
+
+export function applyActiveStagingTraceToTeamData<T extends object>(
+  teamData: T,
+  input: ActiveStagingEngineInput,
+): TeamDataWithActiveStagingTrace<T> {
+  return {
+    ...teamData,
+    competitiveVerificationState: 'staging-controlled',
+    expectedActiveV2SetsResolvedFromMongo: [...input.expectedActiveV2SetsResolvedFromMongo],
+    expectedActiveV2SetsPresentedToEngine: [...input.expectedActiveV2SetsPresentedToEngine],
+    expectedActiveV2SetsAppliedToTeamData: [...input.expectedActiveV2SetsPresentedToEngine],
+    localPilotFallbackUsed: false,
+  };
+}
